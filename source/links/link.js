d1d3624cd81f4fa985226dcbaf35b0f1531f5998
Misaka13514 <Misaka13514@gmail.com>
2023-09-30 20:30:38 +0800

    edit page: update css and js

--- source/links/link.js
+++ source/links/link.js
@@ -1,20 +1,34 @@
 link = {
-  init: function() {
-      var that = this;
-      $.getJSON("https://cdn.jsdelivr.net/gh/Misaka13514/Friends@main/linklist.json",
-      function(data) {
-          that.render(data);
+  init: function () {
+    this.fetchData();
+  },
+  fetchData: function () {
+    fetch("https://cdn.jsdelivr.net/gh/Misaka13514/Friends@main/linklist.json")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        link.render(data);
+      })
+      .catch(function (error) {
+        console.error('Error fetching data:', error);
       });
   },
-  render: function(data) {
-      var html, nickname, avatar, site, li = "";
-      for (var i = 0; i < data.length; i++) {
-          nickname = data[i].nickname;
-          avatar = data[i].avatar;
-          site = data[i].site;
-          li += '<div class="card">' + '<a href="' + site + '" target="_blank" rel="external noopenner nofollow">' + '<div class="thumb" style="background: url( ' + avatar + ');">' + '</div>' + '</a>' + '<div class="card-header">' + '<div><a href="' + site + '" target="_blank">' + nickname + '</a></div>' + '</div>' + '</div>';
-      }
-      $(".link-navigation").append(li);
+  render: function (data) {
+    var html = data.map(function (item) {
+      return `
+        <div class="card">
+          <a href="${item.site}" target="_blank" rel="external noopenner nofollow">
+            <div class="thumb" style="background: url(${item.avatar});"></div>
+          </a>
+          <div class="card-header">
+            <div><a href="${item.site}" target="_blank">${item.nickname}</a></div>
+          </div>
+        </div>`;
+    }).join('');
+
+    document.querySelector(".link-navigation").innerHTML = html;
   }
-}
+};
+
 link.init();
